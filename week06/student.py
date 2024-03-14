2ebfbca46de557a9e2bf2b972391ac35a4554d87
Gustavo Fernandes <G00425776@atu.ie>
2024-03-14 20:05:50 +0000

    Week 6

--- week06/student.py
+++ week06/student.py
@@ -1,10 +1,4 @@
-'''What would you like to do?
- (a) Add new student
- (v) View students
- (q) Quit
-Type one letter (a/v/q):d
-you chose d
-'''
+import sys
 
 def displayMenu():
     print("What would you like to do?")
@@ -13,5 +7,66 @@ def displayMenu():
     print("\t(q) Quit")
     choice = input("Type one letter (a/v/q): ").strip()
     return choice
-choice = displayMenu()
-print(f"You chose {choice}")
+
+students= []
+def readModules():
+    modules = []
+    moduleName = input("\tEnter the first module name: ").strip()
+
+    while moduleName != "":
+        module = {}
+        module["name"] = moduleName
+        module["grade"] = int(input("\tEnter grade: "))
+        modules.append(module)
+        moduleName = input("\tEnter the next module name: ".strip())
+    return modules
+def doAdd(students):
+    currentStudent = {}
+    currentStudent["name"] = input("Enter name: ")
+    currentStudent["modules"] = readModules()
+    students.append(currentStudent)
+    print(students)
+   
+def displayModules(modules):
+    print("\tName \tGrade")
+    for module in modules:
+        print(f"\t{ module['name']} \t{ module['grade']}")
+def doView(students):
+    for currentStudent in students:
+        print(currentStudent["name"])
+        displayModules(currentStudent["modules"])
+    
+def doNothing(dumby):
+    pass
+
+choiceMap = {
+'a': doAdd,
+'v': doView,
+'q': doNothing 
+}
+
+students = []
+selection = displayMenu()
+while(selection != 'q'):
+    if selection in choiceMap:
+        choiceMap[selection](students)
+    else:
+        print("\n\nPlease select either a, v or q")
+    selection = displayMenu()
+
+'''
+
+while(selection != 'q'):
+    if selection == 'a':
+        doAdd()
+    elif selection =='v':
+        doView()
+    elif selection != 'q':
+        print("\n\nPlease select either a, v or q")
+    selection = displayMenu()       
+
+#print(f"You chose {selection}")
+
+'''
+
+
